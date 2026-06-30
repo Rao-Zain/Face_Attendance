f30468e0bb36a95a11047843d9e9fe90a99de050
Muhammad Zain Ul Abdin <167095796+Rao-Zain@users.noreply.github.com>
2026-06-30 15:32:31 +0500

    Add database configuration file

---
+++ config/database.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'host' => 'reseau.proxy.rlwy.net',
+    'port' => 35497,
+    'database' => 'railway',
+    'username' => 'root',
+    'password' => 'bNslUpJZmnjWqAzSNgVxPPoqxhKrluXG',
+    'charset' => 'utf8mb4',
+];
