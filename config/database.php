7887e2ec1a0a4e0021c3544875a6a14e7af4e2f1
Rao Zain <raozn14112001@gmail.com>
2026-06-30 12:59:49 +0500

    Remove exposed secrets from config

--- config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'host' => 'reseau.proxy.rlwy.net',
-    'port' => 35497,
-    'database' => 'railway',
-    'username' => 'root',
-    'password' => 'bNslUpJZmnjWqAzSNgVxPPoqxhKrluXG',
-    'charset' => 'utf8mb4',
-];
